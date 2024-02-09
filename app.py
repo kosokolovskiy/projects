bb75db9fe79cccd71eda3ca019b63eccda0f9763
Konstantin <kosokolovsky@icloud.com>
2024-02-09 05:56:41 +0100

    1

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import streamlit.components.v1 as stc
+import os
+import pandas as pd
+
+def main_app():
+    st.title("Welcome to Streamlit App of Konstantin Sokolovskiy's Projects")
+
+if __name__ == '__main__':
+    main_app()
