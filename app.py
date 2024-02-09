f137dd662bd4a527b04e9d0ee74faa725e0cbc76
Konstantin <kosokolovsky@icloud.com>
2024-02-09 07:55:20 +0100

    tutor work

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ import pandas as pd
 def main_app():
     st.title("Welcome to Streamlit App of Konstantin Sokolovskiy's Projects")
 
+
+    st.markdown('''          
+            "Welcome to my site! 
+            
+            My name is Konstantin Sokolovskiy. As someone on the journey to becoming a specialist in artificial intelligence, I am deeply fascinated by the power of machine learning and data analytics. 
+            My journey began at Bauman Moscow State Technical University, where I studied from 2012 to 2018, focusing on gas turbine engines and unconventional 
+            power plants. Realizing my true calling lay elsewhere, I moved to Germany in 2020 to pursue a Master's in Computational Engineering. 
+            It was here that I fell in love with AI, fascinated by its capabilities and the hands-on implementation process. I try to evolve in this direction in all possble ways: read articles, write, calculate and implement by hand and so on.
+            Since 2014, I've also been tutoring in computer science, mathematics, and physics, successfully preparing students for their final school exams. More on this can be found in the next section."
+    ''')
+
 if __name__ == '__main__':
     main_app()
