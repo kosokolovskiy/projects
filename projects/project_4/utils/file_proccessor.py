cdd948ca516dd7f342f4b25ec37a96fe8e1eca98
Konstantin <kosokolovsky@icloud.com>
2024-03-11 20:50:33 +0100

    cached data

--- projects/project_4/utils/file_proccessor.py
+++ projects/project_4/utils/file_proccessor.py
@@ -34,6 +34,7 @@ class File_Proccessor:
         self.where_to_store_audio = f'{self.app_name}/{self.unique_video_id}/audios/{self.video_name}.{self.extension_audio}'
         # st.info(self.where_to_store_audio)
         self.temp_dir = Path('/mount/src/projects')
+        print(list(self.temp_dir.iterdir()))
         self.info(list(self.temp_dir.iterdir()))
 
 
